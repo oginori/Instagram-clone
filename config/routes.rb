8956be7e6bf5602907b3c2d30976de19b071822e
noriko ogihara <aloha.nori1104@gmail.com>
2020-09-07 19:10:20 +0900

    usersテーブルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: "pictures#index"
   resources :users
 end
--- db/migrate/20200907095744_create_users.rb
+++ db/migrate/20200907095744_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.text :profile_image
       t.timestamps
     end
   end
