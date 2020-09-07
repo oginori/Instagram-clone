6426966a12c49cf304705eb264fd64da2649b11e
noriko ogihara <aloha.nori1104@gmail.com>
2020-09-07 19:34:23 +0900

    セキュアパスワードを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :pictures
   before_validation [ email.downcase! ]
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
