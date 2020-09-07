a23fbbfa2a6556e89c40ac748cf4bbe27e6c9e59
noriko ogihara <aloha.nori1104@gmail.com>
2020-09-07 19:28:40 +0900

    emailのユニーク設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :pictures
+  before_validation [ email.downcase! ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_101956) do
+ActiveRecord::Schema.define(version: 2020_09_07_102523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_101956) do
     t.text "profile_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "pictures", "users"
