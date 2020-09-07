054416cf23ff28039fa40f013e74c901510e8262
noriko ogihara <aloha.nori1104@gmail.com>
2020-09-07 18:51:08 +0900

    config/application.rbを修正, DBを作成

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Instagramclone
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locate = :ja
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators do |g|
