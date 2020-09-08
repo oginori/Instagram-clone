5c4c7599b5496094b57a01a377b7acf2a9e46741
noriko ogihara <aloha.nori1104@gmail.com>
2020-09-08 18:54:36 +0900

    uninitialized エラー

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,26 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to new_picture_path
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image, :image_cache, :content)
   end
 end
