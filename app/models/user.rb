ef0b61771d3fbe06ab4169761e878cf5f65161fa
noriko ogihara <aloha.nori1104@gmail.com>
2020-09-23 08:49:10 +0900

    お気に入り機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :pictures
+  has_many :favorites, dependent: :destroy
   before_validation { email.downcase! }
   has_secure_password
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
+  mount_uploader :profile_image, ProfileUploader
 end
