af8ce55ee32612a4310b20981e25a906346462ef
noriko ogihara <aloha.nori1104@gmail.com>
2020-09-23 20:36:28 +0900

    お気に入り一覧を作成

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,19 @@
+class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.all
+  end
+
+  def new
+    @favorite = Favorite.new
+  end
+
+  def create
+    @favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り登録しました"
+  end
+
+  def destroy
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り解除しました"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :favorites, only: [:new, :index, :create, :destroy]
 end
