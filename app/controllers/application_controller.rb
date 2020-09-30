64cd42b3cce1ab3cfc1bffd64a68d18babac580a
noriko ogihara <aloha.nori1104@gmail.com>
2020-09-30 21:25:26 +0900

    アクセス制限機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,9 @@ class ApplicationController < ActionController::Base
       redirect_to new_session_path
     end
   end
+
+  def login_judgment
+    redirect_to new_session_path, notice: 'ログインしてください。' unless current_user
+  end
+
 end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,7 @@
 class PicturesController < ApplicationController
+  before_action :login_judgment, only: [:show]
+  before_action :set_picture, only: [:edit, :update, :show, :destroy]
+
   def index
     @pictures = Picture.all
   end
@@ -26,11 +29,12 @@ class PicturesController < ApplicationController
   end
 
   def edit
-    @picture = Picture.find(params[:id])
+    if current_user.id != @picture.user.id
+      redirect_to pictures_path
+    end
   end
 
   def update
-    @picture = Picture.find(params[:id])
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: "Successfully edited your post!"
     else
@@ -39,12 +43,10 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
     @favorite = current_user.favorites.find_by(picture_id: @picture.id)
   end
 
   def destroy
-    @picture = Picture.find(params[:id])
     @picture.destroy
     redirect_to pictures_path, notice: "Successfully deleted!"
   end
@@ -53,5 +55,9 @@ class PicturesController < ApplicationController
 
   def picture_params
     params.require(:picture).permit(:image, :image_cache, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
